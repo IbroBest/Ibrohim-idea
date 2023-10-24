4acbbee1a7e7e4417ab3d8c47a1069cda96d07ab
IbroBest <144906288+IbroBest@users.noreply.github.com>
2023-10-24 18:02:46 +0500

    fjksanflfihbsli

--- src/components/Dynamic.jsx
+++ src/components/Dynamic.jsx
@@ -1,28 +1,29 @@
-import { Box, Container, Heading, Image } from '@chakra-ui/react'
-import React, { useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { Context } from '../context/Context'
-import axios from 'axios'
+import { Box, Container, Heading, Image } from "@chakra-ui/react";
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../context/Context";
+import axios from "axios";
 
 export const Dynamic = () => {
-  const {DynamicItem,setDynamicItem} = useContext(Context)
-  const params = useParams()
-    console.log(params)
+  const { DynamicItem, setDynamicItem } = useContext(Context);
+  const params = useParams();
+  console.log(params);
 
-    useEffect(() => {
-      axios
-        .get(`https://api.idea.uz/api/v2/products/6403/attributes`)
-        .then((res) => console.log(res.data.data))
-        .catch((error) => console.log(error));
-    },[]);
+  useEffect(() => {
+    axios
+      .get(`https://api.idea.uz/api/v2/products/6403/attributes`)
+      .then((res) => console.log(res.data.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <Box>
-      <Container maxW={{ sm:'550px',md: "750px", lg: "1116px", xl: "1576px" }}>
-
-        <Image src={DynamicItem.img}/>
+      <Container
+        maxW={{ sm: "550px", md: "750px", lg: "1116px", xl: "1576px" }}
+      >
+        <Image src={DynamicItem.img} />
         <Heading>chiqdi</Heading>
       </Container>
     </Box>
-  )
-}
+  );
+};
